4976b7fae6e14e8e7fa259728818e9fd4b74b3d8
Shane Mc Cormack <shanemcc@gmail.com>
2018-05-21 01:01:30 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-    $usernames = array();
-    $safeusernames = array();
+$usernames = array();
+$safeusernames = array();
 
-    if (file_exists(__DIR__ . '/config.local.php')) {
-        include(__DIR__ . '/config.local.php');
-    }
+if (file_exists(__DIR__ . '/config.local.php')) {
+  include(__DIR__ . '/config.local.php');
+}
