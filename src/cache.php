fe70014b4f2b88f223cd7969f94240b6609b6161
Greg Holmes <git@greg.holmes.name>
2018-05-22 18:54:33 +0000

    Switch to PHP_EOL rather than \r\n

--- src/cache.php
+++ src/cache.php
@@ -8,12 +8,12 @@ $dir = realpath(dirname(__FILE__));
 $cachetime = 30;
 $instagram = new \InstagramScraper\Instagram();
 
-echo "\r\n".date("Y-m-d H:i:s").' Checking files to cache.'."\r\n";
+echo date("Y-m-d H:i:s").' Checking files to cache.'.PHP_EOL;
 foreach ($usernames as $username) {
         $filename = $dir.'/../cache/'.$username;
         $checktime = strtotime ('-'.$cachetime.' minutes');
         if (!file_exists($filename) || filemtime($filename) - $checktime <= $cachetime) {
-		echo date("Y-m-d H:i:s")." Caching: ".$username."\r\n";
+		echo date("Y-m-d H:i:s")." Caching: ".$username.PHP_EOL;
                 $response = $instagram->getMedias($username, 10);
                 file_put_contents($filename, serialize($response));
         }
@@ -22,11 +22,11 @@ foreach ($safeusernames as $username) {
         $filename = $dir.'/../safecache/'.$username;
         $checktime = strtotime ('-'.$cachetime.' minutes');
         if (!file_exists($filename) || filemtime($filename) - $checktime <= $cachetime) {
-		echo date("Y-m-d H:i:s")." Caching: ".$username."\r\n";
+		echo date("Y-m-d H:i:s")." Caching: ".$username.PHP_EOL;
                 $response = $instagram->getMedias($username, 10);
                 file_put_contents($filename, serialize($response));
         }
 }
-echo date("Y-m-d H:i:s").' Finished caching.';
+echo date("Y-m-d H:i:s").' Finished caching.'.PHP_EOL;
 
 ?>
