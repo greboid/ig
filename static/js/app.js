009370c910787476d5f51a4964bca093c48f9a4f
Greg Holmes <git@greg.holmes.name>
2018-07-11 20:48:02 +0000

    Rename frontend -> feed.

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,7 @@ function getImages() {
   offset = $("#app img").length
   $.getJSON('/feed?start='+offset+'&count=19', {}, function(data) {
     $.each(data, function(index, image) {
-      $('#app').append($('<a data-fancybox="images" class="item" title="'+image.source+'" data-source="'+image.source+'" data-caption="'+image.source+' - '+image.shortcode+'<br>'+image.caption+'" href="' + image.url + '"><img class="itemimage" alt="'+image.source+'" src="' + image.thumb + '"/></a>'));
+      $('#app').append($('<a data-fancybox="images" class="item" title="'+image.source+'" data-source="'+image.source+'" data-caption="'+image.source+' - '+image.shortcode+'<br>'+image.caption+'" href="' + image.url + '"><img class="itemimage" src="' + image.thumb + '"/></a>'));
     })
   }).done(function() {
     scrolling = false;
