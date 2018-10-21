b44cf417721517b88c056a0cb8d635ee70e39d9f
Greg Holmes <git@greg.holmes.name>
2018-10-21 18:26:58 +0100

    Third attempt at null safety.

--- src/main/kotlin/instagram.kt
+++ src/main/kotlin/instagram.kt
@@ -1,10 +1,12 @@
 package com.greboid.scraper
 
-import com.google.gson.Gson
+import com.google.gson.*
 import org.jsoup.Jsoup
 import java.io.IOException
+import java.lang.reflect.Type
 import java.net.URL
 import kotlin.streams.toList
+
 
 const val ig: String = "https://www.instagram.com"
 
@@ -17,17 +19,22 @@ fun getProfile(username: String): Profile? {
     val jsonData = doc.select("script:containsData(window._sharedData)").find { element ->
         element.data().startsWith("window._sharedData")
     }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim()
-    val data = Gson().fromJson(jsonData, InstagramSharedData::class.java)
-            .checkProfileData()?.entry_data?.ProfilePage?.firstOrNull()?.graphql?.user ?: return null
+
+    val gson = GsonBuilder()
+            .registerTypeAdapter(User::class.java, AnnotatedDeserializer<User>())
+            .create()
+
+    val data = gson.fromJson(jsonData, InstagramSharedData::class.java)
+            .entry_data.ProfilePage.first().graphql.user
     return Profile(
-            data.username as String,
-            data.id as String,
-            data.biography as String,
+            data.username,
+            data.id,
+            data.biography,
             data.external_url,
             data.profile_pic_url,
             data.profile_pic_url_hd,
-            data.edge_owner_to_timeline_media!!.edges!!.stream().map {
-                getPost(it.node!!.shortcode)
+            data.edge_owner_to_timeline_media.edges.stream().map {
+                getPost(it.node.shortcode)
             }.toList().filterNotNull()
     )
 }
@@ -42,45 +49,16 @@ fun getPost(shortcode: String?): Post? {
         element.data().startsWith("window._sharedData")
     }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim() ?: return null
     val data = Gson().fromJson(jsonData, InstagramSharedData::class.java)
-            .checkPostData()?.entry_data?.PostPage?.firstOrNull()?.graphql?.shortcode_media ?: return null
+            .entry_data.PostPage.first().graphql.shortcode_media
     return Post(
-            data.id as String,
-            when (data.__typename as String) {
-                "GraphImage" -> PostType.IMAGE
-                "GraphSidecar" -> PostType.SIDECAR
-                "GraphVideo" -> PostType.VIDEO
-                else -> PostType.UNKNOWN
-            },
-            data.shortcode as String,
-            data.display_url as URL,
-            data.edge_media_to_caption?.edges?.firstOrNull()?.node?.text as String,
-            data.owner?.id as String,
-            data.owner?.username as String
+            data.id,
+            data.getPostType(),
+            data.shortcode,
+            data.display_url,
+            data.edge_media_to_caption.edges.first().node.text,
+            data.owner.id,
+            data.owner.username
     )
-}
-
-internal fun InstagramSharedData.checkProfileData(): InstagramSharedData? {
-    val entryData: EntryData = this.entry_data ?: return null
-    val user: User = entryData.ProfilePage.firstOrNull()?.graphql?.user ?: return null
-    user.username ?: return null
-    user.id ?: return null
-    user.profile_pic_url_hd = user.profile_pic_url_hd ?: user.profile_pic_url
-    user.edge_owner_to_timeline_media?.edges ?: return null
-    user.edge_owner_to_timeline_media?.edges = user.edge_owner_to_timeline_media?.edges?.stream()
-            ?.filter{it.node?.shortcode != null}?.toList() ?: emptyList()
-    return this
-}
-
-internal fun InstagramSharedData.checkPostData(): InstagramSharedData? {
-    val entryData: EntryData = this.entry_data ?: return null
-    val media: shortcode_media = entryData.PostPage.firstOrNull()?.graphql?.shortcode_media ?: return null
-    media.__typename ?: return null
-    media.shortcode ?: return null
-    media.display_url ?: return null
-    media.edge_media_to_caption?.edges?.firstOrNull()?.node?.text ?: return null
-    media.owner?.id ?: return null
-    media.owner?.username ?: return null
-    return this
 }
 
 class Profile(
@@ -88,7 +66,7 @@ class Profile(
         val id: String,
         val biography: String,
         val external_url: URL?,
-        val profile_pic_url: URL?,
+        val profile_pic_url: URL,
         val profile_pic_url_hd: URL?,
         val posts: List<Post>
 )
@@ -110,77 +88,145 @@ enum class PostType {
     UNKNOWN
 }
 
+internal fun shortcode_media.getPostType(): PostType {
+    return when (__typename) {
+        "GraphImage" -> PostType.IMAGE
+        "GraphSidecar" -> PostType.SIDECAR
+        "GraphVideo" -> PostType.VIDEO
+        else -> PostType.UNKNOWN
+    }
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FIELD)
+internal annotation class JsonRequired
+
+internal class AnnotatedDeserializer<T> : JsonDeserializer<T> {
+
+    @Throws(JsonParseException::class)
+    override fun deserialize(je: JsonElement, type: Type, jdc: JsonDeserializationContext): T {
+        val pojo = Gson().fromJson<T>(je, type) ?: throw IllegalArgumentException("")
+        val fields = (pojo as Any)::class.java.declaredFields
+        for (f in fields) {
+            if (f.getAnnotation(JsonRequired::class.java) != null) {
+                try {
+                    f.isAccessible = true
+                    if (f.get(pojo) == null) {
+                        throw JsonParseException("Missing field in JSON: " + f.name)
+                    }
+                } catch (ex: IllegalArgumentException) {
+                    return pojo
+                } catch (ex: IllegalAccessException) {
+                    return pojo
+                }
+
+            }
+        }
+        return pojo
+    }
+}
+
 internal class InstagramSharedData {
     var rhx_gis: String? = null
-    var entry_data: EntryData? = null
+    @JsonRequired
+    lateinit var entry_data: EntryData
 }
 
 internal class EntryData {
-    var ProfilePage: List<ProfilePage> = emptyList()
-    var PostPage: List<PostPage> = emptyList()
+    @JsonRequired
+    lateinit var ProfilePage: List<ProfilePage>
+    @JsonRequired
+    lateinit var PostPage: List<PostPage>
 }
 
 internal class PostPage {
-    var graphql: postgraphql? = null
+    @JsonRequired
+    lateinit var graphql: postgraphql
 }
 
 internal class postgraphql {
-    var shortcode_media: shortcode_media? = null
+    @JsonRequired
+    lateinit var shortcode_media: shortcode_media
 }
 
 internal class shortcode_media {
-    var id: String? = null
-    var __typename: String? = null
-    var shortcode: String? = null
-    var display_url: URL? = null
-    var edge_media_to_caption: edge_media_to_caption? = null
-    var owner: owner? = null
+    @JsonRequired
+    lateinit var id: String
+    @JsonRequired
+    lateinit var __typename: String
+    @JsonRequired
+    lateinit var shortcode: String
+    @JsonRequired
+    lateinit var display_url: URL
+    @JsonRequired
+    lateinit var edge_media_to_caption: edge_media_to_caption
+    @JsonRequired
+    lateinit var owner: owner
 }
 
 internal class owner {
-    var id: String? = null
-    var username: String? = null
+    @JsonRequired
+    lateinit var id: String
+    @JsonRequired
+    lateinit var username: String
 }
 
 internal class ProfilePage {
-    var graphql: graphql? = null
+    @JsonRequired
+    lateinit var graphql: graphql
 }
 
 internal class graphql {
-    var user: User? = null
+    @JsonRequired
+    lateinit var user: User
 }
 
 internal class User {
-    var edge_owner_to_timeline_media: edge_owner_to_timeline_media? = null
-    var username: String? = null
-    var id: String? = null
-    var biography: String? = null
+    @JsonRequired
+    lateinit var edge_owner_to_timeline_media: edge_owner_to_timeline_media
+    @JsonRequired
+    lateinit var username: String
+    @JsonRequired
+    lateinit var id: String
+    @JsonRequired
+    lateinit var biography: String
     var external_url: URL? = null
-    var profile_pic_url: URL? = null
+    @JsonRequired
+    lateinit var profile_pic_url: URL
     var profile_pic_url_hd: URL? = null
 }
 
 internal class edge_owner_to_timeline_media {
-    var edges: List<nodeHolder>? = null
+    @JsonRequired
+    lateinit var edges: List<nodeHolder>
 
 }
 
 internal class nodeHolder {
-    var node: node? = null
+    @JsonRequired
+    lateinit var node: node
 }
 
 internal class node {
-    var id: String? = null
-    var __typename: String? = null
-    var edge_media_to_caption: edge_media_to_caption? = null
-    var shortcode: String? = null
-    var display_url: URL? = null
-    var thumbnail_src: URL? = null
+    @JsonRequired
+    lateinit var id: String
+    @JsonRequired
+    lateinit var __typename: String
+    @JsonRequired
+    lateinit var edge_media_to_caption: edge_media_to_caption
+    @JsonRequired
+    lateinit var shortcode: String
+    @JsonRequired
+    lateinit var display_url: URL
+    @JsonRequired
+    lateinit var thumbnail_src: URL
 }
 
 internal class edge_media_to_caption {
-    var page_info: page_info? = null
-    var edges: List<captionnodeHolder> = emptyList()
+    @JsonRequired
+    lateinit var page_info: page_info
+    @JsonRequired
+    lateinit var edges: List<captionnodeHolder>
 }
 
 internal class page_info {
@@ -190,9 +236,11 @@ internal class page_info {
 }
 
 internal class captionnodeHolder {
-    var node: captionnode? = null
+    @JsonRequired
+    lateinit var node: captionnode
 }
 
 internal class captionnode {
-    var text: String? = null
+    @JsonRequired
+    lateinit var text: String
 }
