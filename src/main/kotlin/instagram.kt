bb3d5e3484150143b553b34cf196d5dd0aa7e3b8
Greg Holmes <git@greg.holmes.name>
2019-03-31 14:46:30 +0100

    Remove jsoup
    Remove jsoup

--- src/main/kotlin/instagram.kt
+++ src/main/kotlin/instagram.kt
@@ -1,98 +1,107 @@
 package com.greboid.scraper
 
 import com.google.gson.Gson
+import okhttp3.HttpUrl
 import okhttp3.JavaNetCookieJar
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jsoup.Jsoup
 import java.io.IOException
 import java.math.BigInteger
+import java.net.CookieManager
+import java.net.CookiePolicy
 import java.net.URL
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import kotlin.math.min
 import kotlin.streams.toList
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.net.CookiePolicy
-import java.net.CookieManager
 
 const val ig: String = "https://www.instagram.com"
 
 class Instagram {
 
-    companion object {
-        internal fun getURL(url: String): String {
-            val cookieManager = CookieManager().apply {
-                setCookiePolicy(CookiePolicy.ACCEPT_ALL)
-            }
-            val client = OkHttpClient().newBuilder()
-                    .cookieJar(JavaNetCookieJar(cookieManager))
-                    .build()
-            val request = Request.Builder()
-                    .url(url)
-                    .build()
-            return client.newCall(request).execute().use {
-                        it.body()?.string() ?: ""
-                    }
+    internal fun getURL(
+        url: String,
+        params: Map<String, String> = emptyMap(),
+        headers: Map<String, String> = emptyMap()
+    ): String {
+        val cookieManager = CookieManager().apply {
+            setCookiePolicy(CookiePolicy.ACCEPT_ALL)
         }
+        val client = OkHttpClient().newBuilder()
+            .cookieJar(JavaNetCookieJar(cookieManager))
+            .build()
+        val httpBuider = HttpUrl.parse(url)?.newBuilder() ?: return ""
+        for (param in params) {
+            httpBuider.addQueryParameter(param.key, param.value)
+        }
+        val requestBuilder = Request.Builder()
+            .url(httpBuider.build())
+        for (header in headers) {
+            requestBuilder.addHeader(header.key, header.value)
+        }
+        return client.newCall(requestBuilder.build()).execute().use {
+            it.body()?.string() ?: ""
+        }
+    }
 
-        internal fun getShortcodePost(shortcode: String?): Post? {
-            val json = try {
-                val contents = getURL("$ig/p/$shortcode")
-                Jsoup.parse(contents, "$ig/p/$shortcode")
-            } catch (e: IOException) {
-                return null
-            }.select("script:containsData(window._sharedData)").find { element ->
-                element.data().startsWith("window._sharedData")
-            }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim()
-            val data = Gson().fromJson(json, InstagramSharedData::class.java)
-                    .entry_data.PostPage.first().graphql.shortcode_media
-            return Post(
-                    data.id,
-                    data.getPostType(),
-                    data.shortcode,
-                    data.display_url,
-                    when (data.getPostType()) {
-                        PostType.SIDECAR -> data.edge_sidecar_to_children?.edges?.map {
-                            it.node.display_url
-                        }?.toList() ?: emptyList()
-                        PostType.VIDEO -> listOf(data.video_url ?: URL("http://instagram.com"))
-                        else -> listOf(data.display_url)
-                    },
-                    data.edge_media_to_caption.edges.firstOrNull()?.node?.text ?: "",
-                    data.owner.id,
-                    data.owner.username,
-                    data.taken_at_timestamp
-            )
-        }
+    internal fun getShortcodePost(shortcode: String?): Post? {
+        val json = try {
+            val contents = getURL("$ig/p/$shortcode")
+            Jsoup.parse(contents, "$ig/p/$shortcode")
+        } catch (e: IOException) {
+            return null
+        }.select("script:containsData(window._sharedData)").find { element ->
+            element.data().startsWith("window._sharedData")
+        }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim()
+        val data = Gson().fromJson(json, InstagramSharedData::class.java)
+            .entry_data.PostPage.first().graphql.shortcode_media
+        return Post(
+            data.id,
+            data.getPostType(),
+            data.shortcode,
+            data.display_url,
+            when (data.getPostType()) {
+                PostType.SIDECAR -> data.edge_sidecar_to_children?.edges?.map {
+                    it.node.display_url
+                }?.toList() ?: emptyList()
+                PostType.VIDEO -> listOf(data.video_url ?: URL("http://instagram.com"))
+                else -> listOf(data.display_url)
+            },
+            data.edge_media_to_caption.edges.firstOrNull()?.node?.text ?: "",
+            data.owner.id,
+            data.owner.username,
+            data.taken_at_timestamp
+        )
+    }
 
-        internal fun getProfile(username: String): Profile? {
-            val doc = try {
-                val contents = getURL("$ig/$username")
-                Jsoup.parse(contents, "$ig/$username")
-            } catch (e: IOException) {
-                return null
-            }
-            val jsonData = doc.select("script:containsData(window._sharedData)").find { element ->
-                element.data().startsWith("window._sharedData")
-            }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim()
-            val data = Gson().fromJson(jsonData, InstagramSharedData::class.java)
-            val userData = data.entry_data.ProfilePage.first().graphql.user
-            return Profile(
-                    userData.username,
-                    userData.id,
-                    userData.biography,
-                    userData.external_url,
-                    userData.profile_pic_url,
-                    userData.profile_pic_url_hd,
-                    userData.edge_owner_to_timeline_media.edges.stream().map {
-                        getShortcodePost(it.node.shortcode)
-                    }.toList().filterNotNull().toMutableList(),
-                    userData.edge_owner_to_timeline_media.page_info.end_cursor,
-                    userData.edge_owner_to_timeline_media.page_info.has_next_page,
-                    userData.edge_owner_to_timeline_media.count,
-                    data.rhx_gis
-            )
+    internal fun getProfile(username: String): Profile? {
+        val doc = try {
+            val contents = getURL("$ig/$username")
+            Jsoup.parse(contents, "$ig/$username")
+        } catch (e: IOException) {
+            return null
         }
+        val jsonData = doc.select("script:containsData(window._sharedData)").find { element ->
+            element.data().startsWith("window._sharedData")
+        }?.data()?.substringBeforeLast(';')?.substringAfter("=")?.trim()
+        val data = Gson().fromJson(jsonData, InstagramSharedData::class.java)
+        val userData = data.entry_data.ProfilePage.first().graphql.user
+        return Profile(
+            userData.username,
+            userData.id,
+            userData.biography,
+            userData.external_url,
+            userData.profile_pic_url,
+            userData.profile_pic_url_hd,
+            userData.edge_owner_to_timeline_media.edges.stream().map {
+                getShortcodePost(it.node.shortcode)
+            }.toList().filterNotNull().toMutableList(),
+            userData.edge_owner_to_timeline_media.page_info.end_cursor,
+            userData.edge_owner_to_timeline_media.page_info.has_next_page,
+            userData.edge_owner_to_timeline_media.count,
+            data.rhx_gis
+        )
     }
 
     fun getUserProfile(username: String): Profile? {
@@ -113,7 +122,7 @@ class Profile(
         private val count: Int,
         private val rhx_gis: String
 ) {
-    fun backfill(desiredCapacity: Int) {
+    fun backfill(instagram: Instagram, desiredCapacity: Int) {
         val targetCapacity = min(desiredCapacity, count)
         val count = 12
         val fullruns: Int = (targetCapacity - posts.size) / count
@@ -122,9 +131,9 @@ class Profile(
             return
         }
         for (i in fullruns downTo 1) {
-            getOlderData(count)
+            getOlderData(instagram, count)
         }
-        getOlderData(partRuns)
+        getOlderData(instagram, partRuns)
     }
 
     private fun getMD5(value: String): String {
@@ -133,19 +142,22 @@ class Profile(
                 .digest()))
     }
 
-    private fun getOlderData(count: Int) {
-        val soup = Jsoup.connect("$ig/graphql/query/")
-        soup.header("X-Instagram-GIS",
-                getMD5("$rhx_gis:{\"id\":\"$id\",\"first\":$count,\"after\":\"$end_cursor\"}")
+    private fun getOlderData(instagram: Instagram, count: Int) {
+        val json = instagram.getURL(
+            "$ig/graphql/query/",
+            params = mapOf(
+                "query_hash" to "5b0222df65d7f6659c9b82246780caa7",
+                "variables" to "{\"id\":\"$id\",\"first\":$count,\"after\":\"$end_cursor\"}"
+            ),
+            headers = mapOf(
+                "X-Instagram-GIS" to getMD5("$rhx_gis:{\"id\":\"$id\",\"first\":$count,\"after\":\"$end_cursor\"}")
+            )
         )
-        soup.data("query_hash", "5b0222df65d7f6659c9b82246780caa7")
-        soup.data("variables", "{\"id\":\"$id\",\"first\":$count,\"after\":\"$end_cursor\"}")
-        val json = soup.ignoreContentType(true).execute().body()
         val data = Gson().fromJson(json, InstagramData::class.java)
         end_cursor = data.data.user.edge_owner_to_timeline_media.page_info.end_cursor
         hasMore = data.data.user.edge_owner_to_timeline_media.page_info.has_next_page
         posts.addAll(data.data.user.edge_owner_to_timeline_media.edges.stream().map {
-            Instagram.getShortcodePost(it.node.shortcode)
+            instagram.getShortcodePost(it.node.shortcode)
         }.toList().filterNotNull())
     }
 }
