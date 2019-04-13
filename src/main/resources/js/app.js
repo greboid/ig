23b281b2c04d8507c884c748a7cbacae00c1edb3
Greg Holmes <git@greg.holmes.name>
2019-04-13 02:00:15 +0100

    Remove pointless string operations.

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -29,11 +29,11 @@ $(document).keyup(function(e) {
 
 $(document).keyup(function(e) {
      if (e.key === "ArrowLeft") {
-     var hash = $(location).attr('hash').substr(1)
-         if (hash == 'close' || hash == '') {
+     var hash = $(location).attr('hash')
+         if (hash == '#close' || hash == '#') {
              return
          }
-         var box = $('#'+hash).closest('.post').prev('.post').find('.perfundo__overlay').attr('id')
+         var box = $(hash).closest('.post').prev('.post').find('.perfundo__overlay').attr('id')
          if (box !==  void 0) {
             window.location.hash = box
          } else {
@@ -44,11 +44,11 @@ $(document).keyup(function(e) {
 
 $(document).keyup(function(e) {
      if (e.key === "ArrowRight") {
-        var hash = $(location).attr('hash').substr(1)
-        if (hash == 'close' || hash == '') {
+        var hash = $(location).attr('hash')
+        if (hash == '#close' || hash == '#') {
             return
         }
-        var box = $('#'+hash).closest('.post').next('.post').find('.perfundo__overlay').attr('id')
+        var box = $(hash).closest('.post').next('.post').find('.perfundo__overlay').attr('id')
         if (box !== void 0) {
             window.location.hash = box
         } else {
