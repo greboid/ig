9277998f1991d8533d318985c48e3cca9bf14e52
Greg Holmes <git@greg.holmes.name>
2019-05-28 02:35:05 +0100

    Tidy up

--- front/src/MenuBar.js
+++ front/src/MenuBar.js
@@ -3,28 +3,26 @@ import { Link } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap'
 import useAuthContext from './useAuthContext'
 
-function MenuBar() {
+export default function MenuBar() {
 	const { authed, setLoggedOut } = useAuthContext();
-	    return (
-	      <React.Fragment>
-	      	<Navbar>
-			  <Navbar.Brand>IG</Navbar.Brand>
-			  <Nav className="mr-auto">
-			    <Nav.Link as={Link} to='/'>Home</Nav.Link>
-			  </Nav>
-			  <Nav>
-			  	{authed ?
-			  		<React.Fragment>
-				  		<Nav.Link as={Link} to='/admin'>Admin</Nav.Link>
-				  		<Nav.Link as={Link} to='#' onClick={() => setLoggedOut()}>Logout</Nav.Link>
-			  		</React.Fragment>
-			  	:
-			  		<Nav.Link as={Link} to='/login'>Login</Nav.Link>
-			  	}
-			  </Nav>
-			</Navbar>
-	      </React.Fragment>
-	    )
-	}
-
-export default MenuBar;
+    return (
+      <React.Fragment>
+      	<Navbar>
+		  <Navbar.Brand>IG</Navbar.Brand>
+		  <Nav className="mr-auto">
+		    <Nav.Link as={Link} to='/'>Home</Nav.Link>
+		  </Nav>
+		  <Nav>
+		  	{authed ?
+		  		<React.Fragment>
+			  		<Nav.Link as={Link} to='/admin'>Admin</Nav.Link>
+			  		<Nav.Link as={Link} to='#' onClick={() => setLoggedOut()}>Logout</Nav.Link>
+		  		</React.Fragment>
+		  	:
+		  		<Nav.Link as={Link} to='/login'>Login</Nav.Link>
+		  	}
+		  </Nav>
+		</Navbar>
+      </React.Fragment>
+    )
+}
--- front/src/picklist.js
+++ front/src/picklist.js
@@ -2,25 +2,25 @@ import React from 'react';
 import Select from 'react-select';
 
 export default function PickList(props) {
-	return (
-      <ul className="list-group sorted"> 
-           {props.categories.map(function(category){
-              return (
-              	<li className="list-group-item" key={category}>
-              		<p>{category}</p>
-              		<Select
-              			options={props.users}
-              			closeMenuOnSelect={false}
-              			getOptionValue={(option) => (option)}
-              			getOptionLabel={(option) => (option)}
-              			name={category} 
-              			value={props.categoryMap.get(category)}
-              			isMulti
-              			onChange={props.onChange}
-              		/>
-              	</li>
-             	);
-            })}
-        </ul>
-	);
+  return (
+    <ul className="list-group sorted"> 
+      {props.categories.map(function(category){
+        return (
+          <li className="list-group-item" key={category}>
+            <p>{category}</p>
+            <Select
+              options={props.users}
+              closeMenuOnSelect={false}
+              getOptionValue={(option) => (option)}
+              getOptionLabel={(option) => (option)}
+              name={category} 
+              value={props.categoryMap.get(category)}
+              isMulti
+              onChange={props.onChange}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
