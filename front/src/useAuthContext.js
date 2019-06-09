7b7b3142ab24fc3bd7fef6b618f593e5176e1a6f
Greg Holmes <git@greg.holmes.name>
2019-06-09 23:40:58 +0100

    Add background refresh of JWT token

--- front/src/useAuthContext.js
+++ front/src/useAuthContext.js
@@ -1,12 +1,14 @@
 import {useState} from "react";
 import { useSession } from  'react-use-session';
 import createUseContext from "constate";
+import moment from 'moment';
 
 function useAuthState() {
   const { session, save, clear } = useSession('ig')
   var sessionAuthed = false
   var sessionToken = ""
   var sessionExpires = 0
+  var refreshCheckTimeout
   if (session != null && session.token !== null) {
     sessionAuthed = true
     sessionToken = session.token
@@ -20,21 +22,58 @@ function useAuthState() {
   const [authed, setAuthed] = useState(sessionAuthed);
   const [token, setToken] = useState(sessionToken);
   const [expires, setExpires] = useState(sessionExpires);
+  if (sessionAuthed) {
+    refreshCheckTimeout = backgroundCheck(expires, token, setToken, setExpires)
+  }
   const setLoggedIn = (token, expires) => { 
   	setAuthed(true)
   	setToken(token)
   	setExpires(expires)
     save({token: token, expires: expires})
+    if (refreshCheckTimeout === null) {
+      refreshCheckTimeout = backgroundCheck(expires, token, setToken, setExpires)
+    }
   };
   const setLoggedOut = () => {
   	setAuthed(false)
   	setToken("")
   	setExpires(0)
     clear()
+    clearTimeout(refreshCheckTimeout)
   };
-  const getToken = () => { return token };
-  const getExpires = () => { return expires };
-  return { authed, setLoggedIn, setLoggedOut, getToken, getExpires };
+  const getToken = () => { return token }
+  const getExpires = () => { return expires }
+  return { authed, setLoggedIn, setLoggedOut, getToken, getExpires }
+}
+
+function backgroundCheck(expires, token, setToken, setExpires) {
+  var next = moment(expires * 1000).subtract(10, 'minutes').diff(moment())
+  if (next > 0) {
+    return setTimeout(function(){ 
+      renewToken(token, setToken, setExpires)
+    }, next);
+  }
+}
+
+function renewToken(token, setToken, setExpires) {
+  fetch(process.env.REACT_APP_API_URL+'refreshtoken', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+token
+    }
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    if (response.hasOwnProperty("token") && response.hasOwnProperty("expires")) {
+      setToken(response.token)
+      setExpires(response.expires)
+    } else {
+      console.log(`Error: ${response.message}`)
+    }
+  })
 }
 
 const useAuthContext = createUseContext(useAuthState);
