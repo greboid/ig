c09c25375dfef19c479de8306bcac50a6bbd9e5c
Greg Holmes <git@greg.holmes.name>
2019-07-06 14:10:05 +0100

    Destructure props in lightbox

--- front/src/Lightbox.js
+++ front/src/Lightbox.js
@@ -1,41 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import './Lightbox.css'
 import useWindowSize from '@rehooks/window-size'
 import VideoImage from './VideoImage'
 
-const Lightbox = (props) => {
-	const [closed, setClosed] = useState(false)
+const Lightbox = ({src, caption, alt, close}) => {
 	let windowSize = useWindowSize();
 	function handleClick(event) {
-		if (event.target === event.currentTarget) { 
-			props.close()
-		}
+		if (event.target.id === 'lightbox-image' || event.target.id === 'lightbox-caption') { return }
+		close()
 	}
-	function handleKeys(event) {
-		if (event.key = 'Escape') {
-			props.close()
-		}
-	}
-	function renderImage(props) {
-		if (props.src.match(".*mp4.*")) {
-			return <VideoImage id="lightbox-image" src={props.src} />
+	
+	function renderImage(src, alt) {
+		if (src.match(".*mp4.*")) {
+			return <VideoImage id="lightbox-image" src={src} />
 		} else {
-			return <img id="lightbox-image" src={props.src} alt={props.alt} />
+			return <img id="lightbox-image" src={src} alt={alt} />
 		}
 	}
+	useEffect(() => {
+		const handleEscape = (event) => { if (event.key === 'Escape') { close() } }
+		window.addEventListener("keydown", handleEscape)
+		return () => window.removeEventListener("keydown", handleEscape)
+	}, [close]);
 	return(
 		<React.Fragment>
 			<div 
 				id="lightbox"
-				className={props.src === 'undefined' || props.src === "" ? 'lightbox-hidden' : ''} 
+				className={src === 'undefined' || src === "" ? 'lightbox-hidden' : ''} 
 				onClick={handleClick}
-				onKeyDown={handleKeys}
 				tabIndex={0}
 			>
 				<span id="lightbox-close" onClick={handleClick}>&times;</span>
 				<div id="lightbox-content" style={{height: (0.85 * windowSize.innerHeight)}}>
-					{renderImage(props)}
-					<p id="lightbox-caption">{props.caption}</p>
+					{renderImage(src, alt)}
+					<p id="lightbox-caption">{caption}</p>
 				</div>
 			</div>
 		</React.Fragment>
