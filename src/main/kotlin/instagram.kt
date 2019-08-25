980ab05cafd4e47041f326a6f654ee344b4e42d3
Greg Holmes <git@greg.holmes.name>
2019-08-25 01:12:32 +0100

    Add some headers

--- src/main/kotlin/instagram.kt
+++ src/main/kotlin/instagram.kt
@@ -28,6 +28,9 @@ class Instagram {
         setCookiePolicy(CookiePolicy.ACCEPT_ALL)
     })
     private val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0"
+    private val accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    private val acceptLanguage = "en-GB,en;q=0.5"
+    private val acceptEncoding = "en-GB,en;q=0.5"
 
     internal fun getURL(
         url: String,
@@ -47,6 +50,9 @@ class Instagram {
             requestBuilder.addHeader(header.key, header.value)
         }
         requestBuilder.addHeader("User-Agent", userAgent)
+        requestBuilder.addHeader("Accept", accept)
+        requestBuilder.addHeader("Accept-Language", acceptLanguage)
+        requestBuilder.addHeader("Accept-Encoding", acceptEncoding)
         return client.newCall(requestBuilder.build()).execute().use {
             it.body?.string()
         }
