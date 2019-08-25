49e7888800a299eff66feb638a319a9ba76ecae2
Greg Holmes <git@greg.holmes.name>
2019-08-25 01:15:45 +0100

    Fix headers

--- src/main/kotlin/instagram.kt
+++ src/main/kotlin/instagram.kt
@@ -30,7 +30,7 @@ class Instagram {
     private val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0"
     private val accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     private val acceptLanguage = "en-GB,en;q=0.5"
-    private val acceptEncoding = "en-GB,en;q=0.5"
+    private val acceptEncoding = "gzip, deflate"
 
     internal fun getURL(
         url: String,
